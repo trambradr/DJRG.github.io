ced59ec30f913b46faeb9f1defac5191e8431125
佳泽佟 <126307281+trambradr@users.noreply.github.com>
2023-10-08 16:40:36 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,61 @@
+let headerH = document.querySelector("header").clientHeight;
+let part1H = document.querySelector(".part-One").clientHeight;
+let partZeroH = document.querySelector("#part-Zero").clientHeight;
+let partOneH = document.querySelector("#part-One").clientHeight;
+// 对话框
+
+
+
+function showTitle(scrollH) {
+    if (scrollH > 10 && scrollH <= headerH) {
+        // 标题浮现效果
+        var cover = document.getElementById("cover");
+        var title = cover.querySelector("div");
+        title.className = "bigtitle";
+        cover.style = "position=relative"
+        // 标题浮现效果
+    }
+}
+
+function showthat(scrollH) {
+    if (scrollH >= headerH + partZeroH - 10) {
+        // 小标题浮现效果
+        let hd1 = document.getElementById("hd1");
+        let tempBox = hd1.querySelector("div");
+        tempBox.className = "hdTitleIn";
+    }
+    else{
+        let hd1 = document.getElementById("hd1")
+        let tempBox = hd1.querySelector("div");
+        tempBox.className = "hdTitleOut";
+    };
+
+    if (scrollH >= headerH + partZeroH + partOneH +part1H -10){
+        // 小标题浮现效果
+        let hd1 = document.getElementById("hd2");
+        let tempBox = hd1.querySelector("div");
+        tempBox.className = "hdTitleIn";
+    }
+    else{
+        let hd1 = document.getElementById("hd2");
+        let tempBox = hd1.querySelector("div");
+        tempBox.className = "hdTitleOut";
+    };
+}
+
+function bgMove(ele, bg) {
+    var titlepos = GetRect(ele);
+    var tempBox = bg.querySelector("div");
+    if (titlepos.top <= 0) {
+        tempBox.className = "bgTitleIn";
+        if (titlepos.bottom <= Height && titlepos.bottom >= 0) {
+            // 移动的距离
+            // console.log(titlepos.top);
+            bg.style.top = (titlepos.bottom - Height) + "px";
+        }
+    }
+    else {
+        tempBox.className = "bgTitleOut";
+    }
+
+}
